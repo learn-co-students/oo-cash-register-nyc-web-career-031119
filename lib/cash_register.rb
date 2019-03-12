5a1a2d0c459248742279cca77a308172f844ca0e
Yevgeniy <yevgeniyerot@gmail.com>
2019-03-12 19:31:02 -0400

    test

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
 
+class CashRegister
+  def initialize(total = 0)
+    @total = total
+    @discount = 20
+  end
+
+
+  attr_accessor :total
+  attr_accessor :discount
+
+  def add_item(name, price, quantity = 1)
+    self.total += (price * quantity)
+  end
+
+  def apply_discount
+    self.total = (self.total * 0.2).round(0)
+    puts "After the discount, the total comes to $#{self.total}."
+  end
+
+
+
+
+
+
+end
